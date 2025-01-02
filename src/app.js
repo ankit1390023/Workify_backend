900afc9db82c6aa62e7953b08d195ee9b56357c9
Ankit Srivastav <your.email@example.com>
2025-01-02 23:13:00 +0530

    Updated CORS settings in app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 // }));
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: 'https://workify-frontend-8xrbbtvoy-ankit1390023s-projects.vercel.app/',
     credentials: true
 }
 app.use(cors(corsOptions));
