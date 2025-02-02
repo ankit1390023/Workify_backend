4abb61119dbb86a925d68b73aa7b6b016d1165d7
Ankit Srivastav <your.email@example.com>
2025-02-02 19:51:13 +0530

    change

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,6 +3,7 @@ const connectDB = async () => {
     try {
         mongoose.connect(process.env.MONGODB_URL);
         console.log("MONGODB CONNECTED SUCCESSFULLY");
+        
     } catch (error) {
         console.log("MONGODB CONNECTION ID FAILED", error.message);
     }
