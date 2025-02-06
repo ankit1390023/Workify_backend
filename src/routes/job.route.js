5d5f1a13cf8a82a0cfef5ea67f14e41cbfe876b9
Ankit Srivastav <your.email@example.com>
2025-02-06 20:35:45 +0530

    Resolved merge conflict in job.route.js

--- src/routes/job.route.js
+++ src/routes/job.route.js
@@ -6,9 +6,5 @@ const router = Router();
 router.route('/post').post(verifyJwt, postJob);
 router.route('/allJobs').get(getAllJobs);
 router.route('/getJobById/:id').get(getJobById);
-<<<<<<< HEAD
-router.route('/getJobByAdmin').get(verifyJwt,getJobsByAdmin);
-=======
 router.route('/getJobByAdmin').get(getJobsByAdmin);
->>>>>>> a6f537e759bf380ff8afb8ea2e5b47fb326e6092
 export default router;
